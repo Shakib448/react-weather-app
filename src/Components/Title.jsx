f04d1f69f383d72ff07846adfff27c6addef43c9
Shakib448 <shakiba448@gmail.com>
2020-08-12 21:53:11 +0600

    React Weather App

---
+++ src/Components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class Title extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Weather Finder</h1>                
+                <p>Find out temperature, conditions and more...</p>
+            </div>
+        );
+    };
+};
+
+export default Title;
