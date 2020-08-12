8fdc49d021961ff60aa6949d5c32ffbf2ccc597a
Shakib448 <shakiba448@gmail.com>
2020-08-12 22:16:16 +0600

    React Weather App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Title from './Components/Title';
+import Form from './Components/Form';
+import Weather from './Components/Weather';
+
+const API_KEY = "3d280b0f6dmsh93be8d72373e104p16276ajsn81158e8d0d9d";
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Title/>
+                <Form/>
+                <Weather/>
             </div>
         );
     };
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-
-class Title extends React.Component{
+export class Title extends Component {
     render() {
         return (
             <div>
-                <h1>Weather Finder</h1>                
+                <h1>Weather Finder</h1>
                 <p>Find out temperature, conditions and more...</p>
             </div>
-        );
-    };
-};
+        )
+    }
+}
 
 export default Title;
