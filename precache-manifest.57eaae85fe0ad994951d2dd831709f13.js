26201d05641ae33dd195f17a544d2d399c16651e
Shakib448 <shakiba448@gmail.com>
2020-08-13 15:23:40 +0600

    Updates

---
+++ precache-manifest.57eaae85fe0ad994951d2dd831709f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5039a08ed1a68ea9de61a0c7842b3f9c",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "b3cbd1db2f52439cddb9",
+    "url": "/react-weather-app/static/css/main.1dcf1161.chunk.css"
+  },
+  {
+    "revision": "5cdd940b3d9b69335390",
+    "url": "/react-weather-app/static/js/2.57de986b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.57de986b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cbd1db2f52439cddb9",
+    "url": "/react-weather-app/static/js/main.6790f55a.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-app/static/media/bg.b6796c73.jpg"
+  }
+]);
