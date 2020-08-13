2b4671a838feb12c4e1e41b040f3d7e9cdcb180b
Shakib448 <shakiba448@gmail.com>
2020-08-13 14:44:03 +0600

    React Weather App

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
         const data = await api_call.json();
         console.log(data);
         if (city && country) {
